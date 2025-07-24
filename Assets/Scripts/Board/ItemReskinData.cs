d4be14aae251e320b2ff1eb03f6b867c1c47e019
Manh <manhdeptaivodich@gmail.com>
2025-07-25 01:05:39 +0700

    Task 1 -> 3

---
+++ Assets/Scripts/Board/ItemReskinData.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemReskinData", menuName = "Item/Reskin Data")]
+public class ItemReskinData : ScriptableObject
+{
+    public string pathPrefab;
+    [System.Serializable]
+    public class ItemSpritePair
+    {
+        public string itemName;
+        public Sprite newSprite;
+    }
+
+    public ItemSpritePair[] itemSprites;
+}
